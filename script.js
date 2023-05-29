919d4d72f100420ab939ea5184594ead63bd300f
matteo-didone <matteo.dido9@gmail.com>
2023-05-29 17:16:09 +0200

    Finally used correct JS code

--- script.js
+++ script.js
@@ -12,42 +12,42 @@ const teamMembers =
     {
         name: "Wayne Barnett",
         role: "Founder & CEO",
-        photo: "wayne-barnett-founder-ceo.jpg"
+        photo: "img/wayne-barnett-founder-ceo.jpg"
     },
     
     // Angela Caroll
     {
         name: "Angela Caroll",
         role: "Chief Editor",
-        photo: "angela-caroll-chief-editor.jpg"
+        photo: "img/angela-caroll-chief-editor.jpg"
     },
 
     // Walter Gordon
     {
         name: "Walter Gordon",
         role: "Office Manager",
-        photo: "walter-gordon-office-manager.jpg"
+        photo: "img/walter-gordon-office-manager.jpg"
     },
 
     // Angela Lopez
     {
         name: "Angela Lopez",
         role: "Social Media Manager",
-        photo: "angela-lopez-social-media-manager.jpg"
+        photo: "img/angela-lopez-social-media-manager.jpg"
     },
 
     // Scott Estrada
     {
         name: "Scott Estrada",
         role: "Developer",
-        photo: "scott-estrada-developer.jpg"
+        photo: "img/scott-estrada-developer.jpg"
     },
 
     // Barbara Ramos
     {
         name: "Barbara Ramos",
         role: "Graphic Designer",
-        photo: "barbara-ramos-graphic-designer.jpg"
+        photo: "img/barbara-ramos-graphic-designer.jpg"
     }
 ]
 
@@ -66,4 +66,34 @@ for (let i = 0; i < teamMembers.length; i++)
     // Print a line to separate each member's information.
     console.log("_________________________");
 }
+
+// Print to the DOM, for each team member, the name, role, and the photo string.
+for (let i = 0; i < teamMembers.length; i++) 
+{
+    // Create variable to store the current member.
+    const member = teamMembers[i];
+
+    // Get the card element corresponding to the current team member
+    const card = document.querySelectorAll(".card")[i];
+
+    // Get the card-info div element corresponding to the current team member
+    const cardInfo = card.querySelector(".card-info");
+
+    // Update the card's HTML content with the team member's information
+    cardInfo.querySelector("h4").textContent = member.name;
+    cardInfo.querySelector("p").textContent = member.role;
+
+    // Create an image element and set its source to the team member's photo
+    const image = document.createElement("img");
+
+    // Set the image's source to the team member's photo
+    image.src = member.photo;
+
+    // Set the image's alt attribute to the team member's name
+    image.alt = member.name;
+
+    // Append the image element to the card's card-img div
+    card.querySelector(".card-img").appendChild(image);
+}
+
 

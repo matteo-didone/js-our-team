f4358cb531191e24957dfb9908f079f9397f8388
matteo-didone <matteo.dido9@gmail.com>
2023-05-29 16:02:18 +0200

    Created array of objects

---
+++ script.js
@@ -0,0 +1,58 @@
+// Using the provided data, create an array of objects to represent the team members.
+// Each member is characterized by the following information: name, role, and photo.
+// MILESTONE 0:
+// Create the array of objects with the given information.
+// MILESTONE 1:
+// Print to the console, for each team member, the name, role, and the photo string.
+// MILESTONE 2:
+// Print the same information on the DOM as strings.
+// BONUS 1:
+// Convert the photo string into an actual image.
+// BONUS 2:
+// Organize individual members into cards.
+
+// Create an array of literal objects to represent the team members.
+const teamMembers = 
+[
+    // Wayne Barnett
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg"
+    },
+    
+    // Angela Caroll
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg"
+    },
+
+    // Walter Gordon
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg"
+    },
+
+    // Angela Lopez
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg"
+    },
+
+    // Scott Estrada
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "scott-estrada-developer.jpg"
+    },
+
+    // Barbara Ramos
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg"
+    }
+]
